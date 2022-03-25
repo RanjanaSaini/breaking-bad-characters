497ce591443f135e9a4f546caaf07d060829e7ea
Ranjana Saini <01ranjana.saini@gmail.com>
2022-03-25 16:33:42 +0530

    Update Charactergrid.jsx

--- src/components/Charactergrid.jsx
+++ src/components/Charactergrid.jsx
@@ -3,9 +3,9 @@ import Characteritem from './Charcteritem';
 
 function Charactergrid({items , islodding}){
 return(
-islodding ? <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <h1> Lodding </h1>
-</div>  : <section className='cards'>
+islodding ?
+<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}><h1> Lodding </h1> </div>  : 
+ <section className='cards'>
 {items.map(item=>(
  <Characteritem key={item.char_id} item={item}></Characteritem>
   
